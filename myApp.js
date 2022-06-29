548c39d600436e53433bf1d33d161b43d1c7786b
Benjamin Heidebrink <bgheidebrink@gmail.com>
2022-06-29 12:00:41 -0700

    testing sending dirname to a public index

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ let express = require('express');
 let app = express();
 express[
   console.log("Hello World")];
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+  res.send("Hello Express");
+});
 
 
 
