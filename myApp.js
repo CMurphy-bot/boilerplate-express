11601acac83584a50ff1342b1e2799fb1b993eef
Benjamin Heidebrink <bgheidebrink@gmail.com>
2022-07-05 10:59:43 -0700

    Changes

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require('express');
 
+require("dotenv").config();
+
 let app = express();
 
 express[console.log("Hello World")];
@@ -8,11 +10,34 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+function simpleLogger(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+function timeLogger(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}
+
+app.get("/:word/echo", (req, res) => {
+  res.json({echo: req.params.word});
+});
+
 app.use("/public", express.static(__dirname +  "/public"));
+app.use(simpleLogger);
+
+app.get("/now", timeLogger, (req, res) => {
+  res.json({ time: req.time});
+});
 
 app.get("/json", (req, res) => {
-  res.json({
-    "message": "Hello json"
-  });
-  });
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ "message": message });
+});
+
 module.exports = app;
